404d31e00efbadb93631484d5a87b8520709f5ae
Andrew Clarkson <andrew@clarkson.mn>
2014-11-08 13:25:33 -0600

    register urls

--- bellchamber/urls.py
+++ bellchamber/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'bellchamber.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^catalogue', include('products.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
